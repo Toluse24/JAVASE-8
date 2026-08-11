aa891998a27cce0df739179848f6a850ff9ed7b2
Dannii-web <danielajayi599@gmail.com>
2026-08-11 11:48:36 +0100

    Ammends

--- src/GalToLit.java
+++ src/GalToLit.java
@@ -1,13 +1,21 @@
 public class GalToLit {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         double gallons;
         double litres;
+        int counter;
+
+        //converting gallons to litres
+        /*litres = 3.7854;
+
+        counter = 0;
+
+        for (gallons=1; gallons<=10; gallons++)
+            System.out.println((int)gallons + "gallons is approximately " + litres + "litres");*/
+
+        //converting a specific amount to litres
 
         gallons = 10;
-
         litres = gallons * 3.7854;
-
-        System.out.println(gallons + " gallons is " + litres + " litres");
-
+        System.out.println(gallons + " gallons is equivalent to " + litres + "litres");
     }
 }
