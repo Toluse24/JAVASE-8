a2c309cd5a42b49ca8bb364ce6c940ce150210d5
Dannii-web <danielajayi599@gmail.com>
2026-08-21 13:44:21 +0100

    wk(4)-commits

---
+++ tests/Fragment.java
@@ -0,0 +1,12 @@
+package tests;
+
+class Fragment {
+    public static void main(String args[]) {
+
+        for (int i =0; i<10; i++) {
+            System.out.print(i + " ");
+            if (i%2 == 0) continue;
+            System.out.println();
+        }
+    }
+}
