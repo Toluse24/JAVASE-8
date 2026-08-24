b7b20c8debb6ab0d2a8e1221eced7c33459ae0fd
Dannii-web <danielajayi599@gmail.com>
2026-08-24 11:27:23 +0100

    ammends

--- test/ForCount.java
+++
@@ -1,9 +0,0 @@
-public class ForCount {
-    public static void main(String args[]) {
-        int i;
-
-        for (i = 1000; i >= 0; i-=2) {
-            System.out.println(i);
-        }
-    }
-}
